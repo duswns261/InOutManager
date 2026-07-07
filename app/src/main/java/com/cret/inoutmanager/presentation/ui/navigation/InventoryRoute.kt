6d931fc646b4b3bae3787ec927a46cf757ddabd1
Cret <duswns261@naver.com>
2026-07-07 10:44:25 +0900

    feat(#12): introduce inventory navigation routes

---
+++ app/src/main/java/com/cret/inoutmanager/presentation/ui/navigation/InventoryRoute.kt
@@ -0,0 +1,7 @@
+package com.cret.inoutmanager.presentation.ui.navigation
+
+sealed class InventoryRoute(val route: String) {
+    data object Inbound : InventoryRoute("inbound")
+    data object Outbound : InventoryRoute("outbound")
+    data object Status : InventoryRoute("status")
+}
